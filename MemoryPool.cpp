35385a810b87064887430cb0b8d7c1c9912cb92c
matrixkoo <matrixkook@outlook.com>
2019-12-10 16:40:01 +0800

    recode

---
+++ MemoryPool.cpp
@@ -0,0 +1,53 @@
+#include<vector>
+#include<string>
+using int_L = int;
+using char_L = char;
+using str_L = const char*;
+using  var_id = unsigned long long;
+
+int g_var_counter = 4;
+
+
+enum type_id
+{
+	id_int_L = 1,
+	id_char_L,
+	id_str_L,
+	id_var_id
+};
+
+
+
+class memory_pool
+{
+
+	memory_pool()
+	{
+		
+	};
+	memory_pool(memory_pool& i) = delete;
+
+	////////////////////////////////////////////////////////////
+	//				for each type , i will give 
+	//				vector to store
+	//
+	////////////////////////////////////////////////////////////
+
+	unsigned int var_counter;
+
+	template<typename var_type>
+	std::vector<var_type>* MakeVec()
+	{
+		auto pnew_vec = new std::vector<var_type>;
+		return pnew_vec;
+	}
+	void MemoryInit()
+	{
+		ptrs_to_var_vec.push_back(MakeVec<int>());
+		ptrs_to_var_vec.push_back(MakeVec<char>());
+		ptrs_to_var_vec.push_back(MakeVec<std::string>());
+	}
+public:
+	std::vector<void*> ptrs_to_var_vec;
+};
+
--- test.cpp
+++ test.cpp
@@ -1,23 +1,13 @@
-#include "C_Lambda++.h"
-
-////////////////////////////////
-//			
-//       hello world
-//
-///////////////////////////////
-ST_Lambda_CPP;
-
-
-void Lambda()
+#include "MemoryPool.h"
+#include <iostream>
+int main()
 {
-	(
-	[]
-	{
-			Fuc::Exprt("hello", "%s");
-	}
-	___;
-
-	
-
+	memory_pool A;
+	auto a = A.ptrs_to_var_vec[0];
+	auto b =   static_cast<std::vector<int>* >(a);
+	auto &c = *b;
+	c.push_back(233);
+	std::cout << (*b)[0];
+	getchar();
+	return 0;
 }
-
